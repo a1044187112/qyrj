b6641f45129ef8dd1b9bb64f88136e20e28501e6
login_du <1044187112@qq.com>
2018-10-10 12:45:47 +0800

    解析json 添加到数据库

--- nodeJS/utils/connectionDB.js
+++ nodeJS/utils/connectionDB.js
@@ -1,34 +1,74 @@
 var mysql = require("mysql"); 
-
+var fs = require('fs'); //文件模块
 var db = {};
 var connection = mysql.createConnection({  // 创建连接  确定参数
-	host : "localhost",
+	host : "39.108.82.221",
 	user : "root",
-	password : "gzqyrj123456",
-	database : "qyrj",
+	password : "123456",
+	database : "nmbb",
 	port : "3306",
 }); 	
-connection.connect(); // 
-db.sqlAddData = function sqlAddData(sql,param,fn){  // 添加
-		
-		connection.query(sql,param,function(error,results,fields){
-			if(error){
-				throw error;
-			}
-			console.log(results);
-			fn(results);	
-		});
-	}
+connection.connect(); // 开始连接
+//db.query = function sqlQuery(sql,param,fn){
+//		
+//		connection.query(sql,param,function(error,results,fields){
+//			if(error){
+//				throw error;
+//			}
+//			console.log(results);
+//			fn(results);	
+//		});
+//	}
 
-db.query = function sqlQuery(sql,fn){
-	connection.query(sql,function(error,results,fields){
-			if(error){
-				throw error;
+db.insert = function sqlInsert(res){
+	//读取json文件
+    fs.readFile('area.json', 'utf-8', function(err, data) {
+        if (err) {
+            res.send('文件读取失败');
+        } else {
+          data = JSON.parse(data);
+          for(var val in data){
+          	var sql = 'INSERT INTO area(`id`,`name`,`parentId`) VALUES(?,?,?)'; 
+			var addSqlParams = [val,data[val].name,"0"];
+			connection.query(sql,addSqlParams,function(error,results,fields){
+				if(error){
+					throw error;
+				}
+				console.log(results);
+			});
+			
+			for(var v in data[val].child){
+				var sql = 'INSERT INTO area(`id`,`name`,`parentId`) VALUES(?,?,?)'; 
+				var addSqlParams = [v,data[val].child[v].name,val];
+				connection.query(sql,addSqlParams,function(error,results,fields){
+					if(error){
+						throw error;
+					}
+					console.log(results);
+				});
+				
+				for(var a in data[val].child[v].child){
+					var sql = 'INSERT INTO area(`id`,`name`,`parentId`) VALUES(?,?,?)'; 
+					var addSqlParams = [a,data[val].child[v].child[a],v];
+					connection.query(sql,addSqlParams,function(error,results,fields){
+						if(error){
+							throw error;
+						}
+						console.log(results);
+					});
+				}
 			}
-			console.log(results);
-			fn(results);	
-		});
+			
+			
+			
+          }
+           
+        }
+    });
+
+
+	
+
 }
- 
 
 module.exports = db;
